dfa37569ad54dd012adb005ebadf25bf6e885c61
urapua katjizeu <vakaakatjizeu@Gmail.com>
2023-10-17 09:24:41 +0000

    modified:   Testing/D4.hs

--- Testing/D4.hs
+++ Testing/D4.hs
@@ -1,6 +1,9 @@
---parametricity
 x :: [String]
 x = ["Pikachu","Bulbasaur","squirtle"]
 --reverse x
-data color  = Red | Green | Blue
-    deriving(Eq,Show)
+-- data colour  = Red | Green | Blue
+--     deriving(Eq,Show)
+
+
+isLeapYear :: Integer -> Bool
+isLeapYear year = year `mod` 4 == 0 &&  year `mod` 100 /= 0 && year `mod` 400 == 0
